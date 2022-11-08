2d592950b44702d2d087a2157b7474e5edded1da
DamyanBG <d.dimitrov.1988@abv.bg>
2022-11-08 22:33:59 +0200

    add list current data

--- resources/weather_prediction.py
+++ resources/weather_prediction.py
@@ -1,10 +1,11 @@
 from flask import request
 from flask_restful import Resource
+from werkzeug.exceptions import BadRequest
+
 from managers.weather_prediction import WeatherPredictionManager
 from schemas.response.weather_prediction import WeatherPredictionResponseSchema
 from schemas.request.weather_prediction import WeatherPredicitionRequestSchema
 from utils.decorators import validate_schema
-from werkzeug.exceptions import BadRequest
 
 
 class ListCreatePrediction(Resource):
